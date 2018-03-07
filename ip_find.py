54781241444e56f3dda1467f88e6b3fb5f100f27
Simon Bertling <simon.bertling@gmx.de>
2018-03-07 11:11:52 +0900

    pings are starte in the background

--- ip_find.py
+++ ip_find.py
@@ -2,9 +2,10 @@
 
 import subprocess
 
-ip = [192,168,1,1]
+ip = [192,168,140,1]
 
 for i in range(1,254):
-    ip_str = str(ip[0]) + "." + str(ip[1]) + "." + str(ip[2]) + "." + str(i)
-    cmd = "ping -n 1 " + ip_str
-    subprocess.call(cmd, shell=True)
+	ip_str = str(ip[0]) + "." + str(ip[1]) + "." + str(ip[2]) + "." + str(i)
+	cmd = "ping -n 1 -w 1000 " + ip_str + "| grep Reply"
+	subprocess.Popen(cmd, shell=True)
+
