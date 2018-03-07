412cf6d217f258682e5d83305c8350a91f6cceaa
Simon Bertling <simon.bertling@gmx.de>
2018-03-07 13:12:08 +0900

    ip_find.py works now much faster, because of parallel execution of ping commands

--- ip_find.py
+++ ip_find.py
@@ -1,11 +1,21 @@
-#!/bin/python
+#!/usr/bin/python3
+"""
+A simple script to ping all ip addresses within the domain.
+Currently only working with unix based interpreters
+"""
 
 import subprocess
+import time
 
-ip = [192,168,140,1]
+ip = [192,168,1,1]
+pid = list()
 
 for i in range(1,254):
 	ip_str = str(ip[0]) + "." + str(ip[1]) + "." + str(ip[2]) + "." + str(i)
-	cmd = "ping -n 1 -w 1000 " + ip_str + "| grep Reply"
-	subprocess.Popen(cmd, shell=True)
+	cmd = "ping -c 1 -W 2 " + ip_str + " | grep -i 'bytes from'"
+	pid.append(subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE))
 
+for i in range(1,254):
+	str = pid[i-1].stdout.read()
+	if str != b'':
+		print(str.decode())
