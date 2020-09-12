d532975cc22f7a80746e06c025f08c57fb6198a4
Simon Bertling <simon.bertling@gmx.de>
2020-09-12 22:27:46 +0200

    added http1.1

--- flask/app/routes.py
+++ flask/app/routes.py
@@ -1,4 +1,5 @@
 from flask import render_template
+from werkzeug.serving import WSGIRequestHandler
 from app import app
 
 @app.route('/')
@@ -6,3 +7,5 @@ from app import app
 def index():
     user = {'username': 'Miguel'}
     return render_template('index.html', title='Home', user=user)
+
+WSGIRequestHandler.protocol_version = "HTTP/1.1"
