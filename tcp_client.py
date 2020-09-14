2a02b932e45fe95fdbc24a7aaf6e86b6b564f6dd
Simon Bertling <berti.x@gmx.de>
2020-09-14 23:44:16 +0200

    updated tcp_client

--- tcp_client.py
+++ tcp_client.py
@@ -1,32 +1,35 @@
 #!/usr/bin/env python
-
 import socket
 import sys
 
-argv = sys.argv
+argv = {}
+for i in range(len(sys.argv)):
+    argv[i] = sys.argv[i]
 
-for i in range(len(argv)):
-	arg = argv[i]
-	print( arg )
-
-TCP_IP = argv[1]
-TCP_PORT = 5025
+TCP_IP = argv.get(1, "localhost")
+TCP_PORT = int(argv.get(2, 8080))
 BUFFER_SIZE = 1024
-MESSAGE1 = b"GET /wiki/Katzen HTTP/1.1\n"
-MESSAGE2 = b"Host: de.wikipedia.de\n\n"
+REQUEST_DEF = f"GET / HTTP/1.1\n"
+REQUEST_DEF += f"Host: {TCP_IP}\n\n"
+REQUEST_DEF += "asdasdasd"*1000
+REQUEST = argv.get(3, REQUEST_DEF)
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect((TCP_IP, TCP_PORT))
+def send_and_receive(host, port, request):
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    s.setsockopt(socket.IPPROTO_TCP, socket.TCP_NODELAY, True)
+    s.connect((host, port))
+    n_bytes_sent = s.send(request.encode("utf-8"))
+    data = ""
+    while True:
+        data_recv = s.recv(BUFFER_SIZE)
+        if data_recv:
+            data += data_recv.decode("utf-8")
+        else:
+            break
+    s.close()
+    return data
 
-sent = s.send(MESSAGE1)
-print(sent)
-sent = s.send(MESSAGE2)
-print(sent)
-
-data = s.recv(BUFFER_SIZE)
-print("closing...")
-asd = s.close()
-print("closed!")
-
-print("received data: %s", data)
-
+for i in range(1):
+    data = send_and_receive(TCP_IP, TCP_PORT, REQUEST)
+    # print(data)
