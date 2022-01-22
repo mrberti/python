17dc5f8da15508f5c86bc499ef0e1fe4b27a07bf
Simon Bertling <berti.x@gmx.de>
2022-01-22 17:33:09 +0100

    updated pins for esp32 board

--- upython/esp32/board.py
+++ upython/esp32/board.py
@@ -9,9 +9,9 @@ __CPU__ = "esp32"
 
 # Pin defines
 LED_PIN_NO = 2
-SDA_PIN_NO = 22
-SCL_PIN_NO = 23
-TX_PIN_NO = 23
+SDA_PIN_NO = 23
+SCL_PIN_NO = 22
+TX_PIN_NO = 21
 # RST_PIN_NO = 
 # SDA = machine.Pin(SDA_PIN_NO, machine.Pin.OUT)
 # SCL = machine.Pin(SCL_PIN_NO, machine.Pin.OUT)
