c11d34b28176ade75b039329104afb3623dd4dc2
Simon Bertling <simon.bertling@gmx.de>
2022-06-17 13:20:56 +0200

    made get_revisions async

--- svn/svn_tools.py
+++ svn/svn_tools.py
@@ -2,11 +2,12 @@
 import os
 import re
 import asyncio
-from itertools import islice
 from datetime import datetime
 from pathlib import Path
 from subprocess import run, PIPE, CalledProcessError
 from xml.etree import ElementTree
+
+from pprint import pprint
 
 wc_path = Path("/home/simon/svn/test")
 f_path = wc_path / "dir/lol.txt"
@@ -50,10 +51,9 @@ async def svn_async(args: list):
     else:
         return stdout
 
-def get_revisions(f_path):
-    xml = svn(["log", "--xml", "-rHEAD:1", str(f_path)])
+def parse_revisions(xml_data):
     revisions = []
-    for logentry in xml.findall(".//logentry"):
+    for logentry in xml_data.findall(".//logentry"):
         revision_number = int(logentry.attrib["revision"])
         author = logentry.find("author").text
         date = datetime.strptime(logentry.find("date").text, "%Y-%m-%dT%H:%M:%S.%f%z")
@@ -67,11 +67,19 @@ def get_revisions(f_path):
         revisions.append(revision)
     return revisions
 
+def get_revisions(f_path):
+    xml = svn(["log", "--xml", "-rHEAD:1", str(f_path)])
+    return parse_revisions(xml)
+
+async def get_revisions_async(f_path):
+    xml = await svn_async(["log", "--xml", "-rHEAD:1", str(f_path)])
+    return parse_revisions(xml)
+
 def parse_contents(files_data, revisions, include_data=True, reg_ex=None):
     contents = []
     for file_data, revision in zip(files_data, revisions):
-        # print(f">>> {f_path}@REV{revision['revision']}")
-        # print(file_data)
+        print(f">>> {f_path}@REV{revision['revision']}")
+        print(file_data)
         content = {
             "file": str(f_path),
             "revision_info": revision,
@@ -102,11 +110,11 @@ async def get_contents_async(
         include_data=True,
         reg_ex=None,
         max_subprocesses=10):
-    revisions = get_revisions(f_path) * 10
+    revisions = await get_revisions_async(f_path)
     files_data = []
-    for q in (revisions[i:i+max_subprocesses] for i in range(0, len(revisions), max_subprocesses)):
+    for revision_slice in (revisions[i:i+max_subprocesses] for i in range(0, len(revisions), max_subprocesses)):
         proc_list = []
-        for revision in q:
+        for revision in revision_slice:
             proc_list.append(
                 svn_async(["cat",  f"-r{revision['revision']}", str(f_path)]),
             )
@@ -114,7 +122,8 @@ async def get_contents_async(
     return parse_contents(files_data, revisions, include_data, reg_ex)
 
 async def main_async():
-    return await get_contents_async(f_path, reg_ex=r"2022-(.*)")
+    contents = await get_contents_async(f_path, include_data=False, reg_ex=r"2022-(.*)")
+    pprint(contents)
 
 def main():
     print(os.getcwd())
@@ -125,5 +134,8 @@ def main():
 
 #%%
 if __name__ == "__main__":
+    start = datetime.now()
     # main()
     asyncio.run(main_async())
+    end = datetime.now()
+    print(f"Required time: {end - start}")
